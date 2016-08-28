37fc45ed93b3b4f487d242eea56e7d15e0a3bfc7
Sean Kee <skee66499@gmail.com>
2016-08-28 11:28:38 -0400

    v0.3.5 Fixed collision boxes for buttons

--- src/initialize.cpp
+++ src/initialize.cpp
@@ -8,7 +8,7 @@
 
 #define WINDOW_WIDTH 1280
 #define WINDOW_HEIGHT 720
-const char *windowTitle = "Astroshark Labs Testbox v0.3.4";
+const char *windowTitle = "Astroshark Labs Testbox v0.3.5";
 
 void loadTextures();
 void close();
