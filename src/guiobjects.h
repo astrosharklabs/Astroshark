47e4f6db874cd68fb7bf91163ac1d5e6b17005f3
MrSkee <skee66499@gmail.com>
2016-09-29 12:59:08 -0400

    Update and rename objects.h to guiobjects.h

---
+++ src/guiobjects.h
@@ -0,0 +1,12 @@
+#include "title.h"
+#include "camera.h"
+#include "buttons.h"
+#include "background.h"
+
+#ifndef objects_h
+extern title title;
+extern button startgame_button;
+extern button option_button;
+extern button credits_button;
+extern button quit_button
+#endif
