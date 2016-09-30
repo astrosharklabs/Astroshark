bb4d150e60596a144a88d190f33161adf42acf69
MrSkee <skee66499@gmail.com>
2016-09-30 16:33:08 -0400

    Update statemanager.cpp

--- src/statemanager.cpp
+++ src/statemanager.cpp
@@ -1,4 +1,20 @@
 #include "statemanager.h"
+#include "constants.h"
 #include "SDL.h"
 //Include States Here
 #include "mainmenu.h"
+
+void initialize_StateManager(SDL_Renderer *) {
+ 
+  while (STATE != QUIT) {
+   switch (STATE) {
+     case MAIN_MENU:
+       mainmenu(renderer);
+       break;
+     case START_GAME:
+       break;
+     case QUIT:
+       break;
+   }
+  }
+}
