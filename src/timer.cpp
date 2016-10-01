e2550db02509433a58cf6a2684af6949b5209e4e
Sean Kee <skee66499@gmail.com>
2016-09-30 23:02:20 -0400

    v0.5.0 Added timer bases, not implemented yet

---
+++ src/timer.cpp
@@ -0,0 +1,73 @@
+#include "SDL.h"
+#include "timer.h"
+
+void timer::setup() {
+	ticksPaused = 0;
+	ticksStarted = 0;
+
+	STARTED = false;
+	PAUSED = false;
+}
+
+void timer::start() {
+	//Resets everything and starts the timer
+	STARTED = true;
+	PAUSED = false;
+
+	ticksStarted = SDL_GetTicks();
+	ticksPaused = 0;
+}
+
+void timer::stop() {
+	STARTED = false;
+	PAUSED = false;
+
+	ticksStopped = SDL_GetTicks() - ticksStarted;
+	ticksStarted = 0;
+	ticksPaused = 0;
+}
+
+void timer::pause() {
+	if (STARTED == true && PAUSED == false) {
+		PAUSED = true;
+
+		ticksPaused = SDL_GetTicks() - ticksStarted;
+		ticksStarted = 0;
+	}
+}
+
+void timer::unpause() {
+	if (STARTED == true && PAUSED == true) {
+		PAUSED = false;
+
+		ticksStarted = SDL_GetTicks() - ticksPaused;
+		ticksPaused = 0;
+	}
+}
+
+Uint32 timer::getTicks() {
+	Uint32 ticks = 0;
+
+	if (STARTED == true) {
+		if (PAUSED == true) {
+			ticks = ticksPaused;
+		}
+		else {
+			ticks = SDL_GetTicks() - ticksStarted;
+		}
+	}
+	
+	return ticks;
+}
+
+Uint32 timer::getStoppedTicks() {
+	return ticksStopped;
+}
+
+bool timer::isStarted() {
+	return STARTED;
+}
+
+bool timer::isPaused() {
+	return PAUSED;
+}
--- src/timer.h
+++ src/timer.h
@@ -1,1 +1,28 @@
 #pragma once
+#include "SDL.h"
+
+#ifndef timer_h
+
+class timer {
+public:
+	void setup();
+	void start();
+	void stop();
+	void pause();
+	void unpause();
+
+	Uint32 getTicks();
+	Uint32 getStoppedTicks(); //Gets last stopped ticks
+
+	bool isStarted();
+	bool isPaused();
+private:
+	Uint32 ticksPaused;
+	Uint32 ticksStarted;
+	Uint32 ticksStopped;
+
+	bool STARTED;
+	bool PAUSED;
+};
+
+#endif // !timer_h
